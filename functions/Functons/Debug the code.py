9d03dc159d937a869c0456a463b2c681690b907b
Abdallah <abdallahmohamedama12@gmail.com>
2025-08-17 16:44:39 +0300

    Add function to print student information with sample data

---
+++ functions/Functons/Debug the code.py
@@ -0,0 +1,45 @@
+def print_students_info(name, age, city, school):
+    print(f"Name: {name.title()}")
+    print(f"Age: {age}")
+    print(f"City: {city.title()}")
+    print(f"School: {school.title()}")
+
+
+print_students_info("Ahmed Mohamed", 25, "Cairo", "Codezilla")
+print_students_info("Mohamed Ahmed", 33, "Cairo", "Al-Azhar")
+print_students_info("Ali Hassan", 30, "Alexandria", "Codezilla")
+print_students_info("Hazem Khaled", 35, "New York", "Khaled ibn al-Walid")
+print_students_info("Hamed Ali", 25, "Tanta", "Al Durra")
+
+
+
+
+
+
+
+
+# Print the following Information for each Student:
+# name: "Ahmed Mohamed",
+# age: 25,
+# city: "Cairo",
+# School: "Codezilla"
+
+# name: "Mohamed Ahmed",
+# age: 33,
+# city: "Cairo",
+# School: "Al-Azhar"
+
+# name: "Ali Hassan",
+# age: 30,
+# city: "Alexandria",
+# School: "Codezilla"
+
+# name: "Hazem Khaled",
+# age: 35,
+# city: "New York",
+# School: "Khaled ibn al-Walid"
+
+# name: "Hamed Ali",
+# age: 25,
+# city: "Tanta",
+# School: "Al Durra"
