370c6d8b1e6fe0cac22735cfb675fae1d8db5dfc
Abdallah <abdallahmohamedama12@gmail.com>
2025-08-17 16:03:57 +0300

    Add function to print pizza items and their prices

---
+++ functions/Functons/ex_lec2_29_functions_2.py
@@ -0,0 +1,14 @@
+# Make a function that prints the items and their prices
+pizzas = {
+"Margherita": 120,
+"Pepperoni": 200,
+"Hawaiian": 150,
+"Meat Lovers": 250,
+"Mushroom": 140,
+}
+
+def print_items_and_prices(items):
+    for item, price in items.items():
+        print(f"{item} Pizza: {price}EGP")
+
+print(print_items_and_prices(pizzas))
