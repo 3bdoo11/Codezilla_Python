bde033abb465166d0e210193a6ce57619bcbe6aa
Abdallah <abdallahmohamedama12@gmail.com>
2025-08-17 16:40:00 +0300

    Add function to print employee information with sample data

---
+++ functions/Functons/debuging.py
@@ -0,0 +1,18 @@
+def print_employee_info(name, age, salary):
+    print(f"Name: {name.title()}")
+    print(f"Age: {age}")
+    print(f"Salary: {salary}")
+# 1.
+print_employee_info("Mohamed ahmed", 25, 10_000)
+#2.
+print_employee_info("hassan Ali", 33, 15_000)
+#3.
+print_employee_info(age=30, name="Ali Hassan",salary= 20_000)
+#4.
+print_employee_info("Ahmed Mohamed", salary=15_000,age= 35)
+#5.
+print_employee_info("Hazem Khaled", 21 ,salary=15_000)
+#6.
+print_employee_info(age= 25, name="Hamed Ali", salary=14_000)
+#7.
+print_employee_info("Mohamed khedr", salary=13_000, age=25)
